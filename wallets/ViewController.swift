07f05b1da16e4d34a2d03e8a091674639c9df5ac
Bogdan Redkin <bogdan.redkin@clearscale.com>
2021-11-09 13:33:18 +0300

    Initial Commit

---
+++ wallets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wallets
+//
+//  Created by Bogdan Redkin on 09.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
